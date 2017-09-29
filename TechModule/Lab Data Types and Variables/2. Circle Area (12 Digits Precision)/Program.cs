5e06a02e3dece6bae34543a2e71f218befdb2bec
Nikismyname <nikismyname@abv.bg>
2017-09-29 18:08:03 +0300

    Adding data types Lab

---
+++ TechModule/Lab Data Types and Variables/2. Circle Area (12 Digits Precision)/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Circle_Area__12_Digits_Precision_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r =double.Parse(Console.ReadLine());
+            Console.WriteLine((Math.PI*r*r).ToString("f12"));
+        }
+    }
+}
---
+++ TechModule/Lab Data Types and Variables/9. Refactor Special Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.Refactor_Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int upperBound = int.Parse(Console.ReadLine());
+            
+            bool isSpecial = false;
+
+            for (int num = 1; num <= upperBound; num++)
+            {
+                int currenNum = num;
+                int digitSum = 0;
+
+                while (currenNum > 0)
+                {
+                    digitSum += currenNum % 10;
+                    currenNum /= 10;
+                }
+
+                isSpecial = (digitSum == 5) || (digitSum == 7) || (digitSum == 11);
+                Console.WriteLine($"{num} -> {isSpecial}");
+            }
+            //Console.ReadLine();
+        }
+    }
+}
