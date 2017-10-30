b36985ee80621d730a5e0e507ef0ffe8a28cc9a3
Nikismyname <nikismyname@abv.bg>
2017-10-30 16:15:01 +0200

    More exams

---
+++ TechModuleExams/Prog. Fund. Sample Exam I - June 2016/04. Population Aggregation/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4
+{
+    class Program
+    {
+        static void Main()
+        {
+            var countries = new Dictionary<string, Dictionary<string, long>>();
+            var repeatTown = new Dictionary<string, int>();
+
+            string prohibited = "@#$&0123456789";
+
+            string input = Console.ReadLine();
+
+            while (input != "stop")
+            {
+                var tokens = input.Split('\\');
+
+                for (int i = 0; i < 2; i++)
+                {
+                    List<int> prohibInd = new List<int>();
+
+                    for (int j = 0; j < tokens[i].Length; j++)
+                    {
+                        if (prohibited.Contains(tokens[i][j]))
+                        {
+                            prohibInd.Add(j);
+                        }
+                    }
+
+                    for (int j = prohibInd.Count - 1; j >= 0; j--)
+                    {
+                        tokens[i] = tokens[i].Remove(prohibInd[j], 1);
+                    }
+                }
+
+                string country = string.Empty;
+                string city = string.Empty;
+
+                if (char.IsUpper(tokens[0].First()))
+                {
+                    country = tokens[0];
+                    city = tokens[1];
+                }
+                else
+                {
+                    country = tokens[1];
+                    city = tokens[0];
+                }
+
+                if (!countries.ContainsKey(country))
+                {
+                    countries[country] = new Dictionary<string, long>();
+                    repeatTown[country] = 0;
+                }
+
+                if (countries[country].ContainsKey(city))
+                {
+                    repeatTown[country]++;
+                }
+
+                countries[country][city] = long.Parse(tokens[2]);
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in countries.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value.Count + repeatTown[item.Key]}");
+            }
+
+            var toPrint = new List<KeyValuePair<string, long>>();
+
+            foreach (var item in countries)
+            {
+                toPrint.AddRange(item.Value);
+            }
+
+            foreach (var item in toPrint.OrderByDescending(x => x.Value).Take(3))
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ TechModuleExams/Prog. Fund. Sample Exam II - June 2016/03. Matrix Operator/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3
+{
+    class Program
+    {
+        static void Main()
+        {
+            long y = long.Parse(Console.ReadLine());
+
+            long[][] table = new long[y][];
+
+            for (int i = 0; i < y; i++)
+            {
+                table[i] = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            }
+
+            string input = Console.ReadLine();
+            while (input != "end")
+            {
+                var tokens = input.Split();
+
+                switch (tokens[0])
+                {
+                    case "remove":
+                        Remove(ref table, tokens);
+                        break;
+                    case "swap":
+                        Swap(ref table, tokens);
+                        break;
+                    case "insert":
+                        Insert(ref table, tokens);
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            for (int i = 0; i < table.Length; i++)
+            {
+                Console.WriteLine(string.Join(" ", table[i]));
+            }
+
+            //Console.ReadLine();
+        }
+
+        public static void Insert(ref long[][] table, string[] tokens)
+        {
+            int row = int.Parse(tokens[1]);
+            long num = int.Parse(tokens[2]);
+
+            List<long> insertion = table[row].ToList();
+            insertion.Insert(0, num);
+            table[row] = insertion.ToArray();
+        }
+
+        public static void Swap(ref long[][] table, string[] tokens)
+        {
+            int rowOne = int.Parse(tokens[1]);
+            int rowTwo = int.Parse(tokens[2]);
+
+            long[] buffer = table[rowOne].ToArray();
+
+            table[rowOne] = table[rowTwo];
+            table[rowTwo] = buffer;
+        }
+
+        public static void Remove(ref long[][] table, string[] tokens)
+        {
+            int num = int.Parse(tokens[3]);
+
+            if (tokens[2] == "row")
+            {
+                switch (tokens[1])
+                {
+                    case "even":
+                        table[num] = table[num].Where(x => Math.Abs(x) % 2 == 1).ToArray();
+                        break;
+                    case "odd":
+                        table[num] = table[num].Where(x => Math.Abs(x) % 2 == 0).ToArray();
+                        break;
+                    //maybe 0 is not positive
+                    case "positive":
+                        table[num] = table[num].Where(x => x < 0).ToArray();
+                        break;
+                    case "negative":
+                        table[num] = table[num].Where(x => x >= 0).ToArray();
+                        break;
+                }
+            }
+
+            if (tokens[2] == "col")
+            {
+
+                Func<long, bool> test = x =>
+                {
+                    throw new ArgumentException();
+                };
+
+                switch (tokens[1])
+                {
+                    case "even":
+                        test = x =>
+                        {
+                            if (Math.Abs(x) % 2 == 1)
+                            {
+                                return true;
+                            }
+                            return false;
+                        };
+                        break;
+                    case "odd":
+                        test = x =>
+                        {
+                            if (Math.Abs(x) % 2 == 0)
+                            {
+                                return true;
+                            }
+                            return false;
+                        }; break;
+                    //maybe 0 is not positive
+                    case "positive":
+                        test = x =>
+                        {
+                            if (x < 0)
+                            {
+                                return true;
+                            }
+                            return false;
+                        };
+                        break;
+                    case "negative":
+                        test = x =>
+                        {
+                            if (x >= 0)
+                            {
+                                return true;
+                            }
+                            return false;
+                        }; break;
+                }
+
+
+                for (int i = 0; i < table.Length; i++)
+                {
+                    //Console.WriteLine($"lenght, desired index: {table[i].Length} {num}");
+
+                    if (table[i].Length >= num + 1)
+                    {
+                        //  Console.WriteLine("here");
+                        if (!test(table[i][num]))
+                        {
+                            // Console.WriteLine("here2");
+                            table[i] = table[i].Where((x, j) => j != num).ToArray();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+/*
+
+ */
